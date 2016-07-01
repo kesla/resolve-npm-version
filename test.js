5e5e9d3fdc151c48deaa8f7b15a23b9d4d2b9055
David Bjorklund <david.bjorklund@gmail.com>
2016-07-01 02:15:24 +0200

    code

---
+++ test.js
@@ -0,0 +1,84 @@
+import test from 'tapava';
+import resolveNpmVersion from './lib/index';
+
+test('dist-tag', function * (t) {
+  const pkg = {
+    'dist-tags': {
+      latest: '1.0.0'
+    },
+    versions: {
+      '2.0.0-beta1': {
+        version: '2.0.0-beta1'
+      },
+      '1.0.0': {
+        version: '1.0.0'
+      },
+      '0.5.0': {
+        version: '0.5.0'
+      },
+      '0.5.1': {
+        version: '0.5.1'
+      }
+    }
+  };
+  const actual = resolveNpmVersion(pkg, 'latest');
+  const expected = {version: '1.0.0'};
+  t.deepEqual(actual, expected);
+});
+
+test('version', function * (t) {
+  const pkg = {
+    versions: {
+      '1.0.0': {
+        version: '1.0.0'
+      },
+      '0.5.0': {
+        version: '0.5.0'
+      },
+      '0.5.1': {
+        version: '0.5.1'
+      }
+    }
+  };
+  const actual = resolveNpmVersion(pkg, '^0.5.0');
+  const expected = {version: '0.5.1'};
+  t.deepEqual(actual, expected);
+});
+
+test('unknown version', function * (t) {
+  const pkg = {
+    versions: {
+      '1.0.0': {
+        version: '1.0.0'
+      },
+      '0.5.0': {
+        version: '0.5.0'
+      },
+      '0.5.1': {
+        versions: '0.5.1'
+      }
+    }
+  };
+  const actual = resolveNpmVersion(pkg, '^0.5.2');
+  const expected = undefined;
+  t.is(actual, expected);
+});
+
+test('unknown tag', function * (t) {
+  const pkg = {
+    versions: {
+      '1.0.0': {
+        version: '1.0.0'
+      },
+      '0.5.0': {
+        version: '0.5.0'
+      },
+      '0.5.1': {
+        versions: '0.5.1'
+      }
+    }
+  };
+  const actual = resolveNpmVersion(pkg, 'none-existing-tag');
+  const expected = undefined;
+  t.is(actual, expected);
+});
